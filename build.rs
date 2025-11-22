7c7a6822a58e9e343c54386c237d6465413cfd8b
ngbal <ngbal@example.com>
2025-11-22 14:08:38 +0700

    Update icon to icon2.ico and add icon to window

--- build.rs
+++ build.rs
@@ -3,7 +3,23 @@ extern crate winres;
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
+        
+        // 1. Giữ nguyên icon của bạn
         res.set_icon("src/icon2.ico");
+        
+        // 2. THÊM MỚI: Nhúng Manifest để bắt buộc chạy Admin
+        res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#);
+
         res.compile().unwrap();
     }
 }
